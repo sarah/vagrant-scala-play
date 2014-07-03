7f17d66a2de2c64d9a6292eebf3a64877a7d083c
sarah <sarahg.gray@gmail.com>
2014-07-02 21:45:03 -0500

    Removes mongo install

--- provision.sh
+++ provision.sh
@@ -1,8 +1,3 @@
-# Configure apt for mongodb
-# http://docs.mongodb.org/manual/tutorial/install-mongodb-on-ubuntu/
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
-
 # Configure apt for java
 # http://www.webupd8.org/2012/01/install-oracle-java-jdk-7-in-ubuntu-via.html
 apt-get update
@@ -13,13 +8,11 @@ echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /u
 # Update apt sources
 apt-get update
 
-# Install stuff available through apt-get
-apt-get install -y unzip wget git vim mongodb-10gen oracle-java7-installer oracle-java7-set-default
-
 # Install scala and play manually
 cd /opt
 wget http://www.scala-lang.org/files/archive/scala-2.10.3.tgz
 tar -zxvf scala-2.10.3.tgz
+
 wget http://downloads.typesafe.com/play/2.2.1/play-2.2.1.zip
 unzip play-2.2.1.zip
 chmod +x /opt/play-2.2.1/play
